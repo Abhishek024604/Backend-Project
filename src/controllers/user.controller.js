0125a177ff3457fa3484ff1eec7f78e26d5626b8
Abhishek Singh <parmarabhishek2404@gmail.com>
2025-01-18 18:13:21 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -457,7 +457,6 @@ export { registerUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    updateUserCoverImage,
     getUserChannelProfile,
     getWatchHistory
  }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import {loginUser,
      logoutUser,
-      refreshAccessToken,
-       registerUser,
-    refreshAccessToken,
-changeCurrentPassword,
-getCurrentUser,
-updateUserAvatar,
-updateUserCoverImage,
-getUserChannelProfile,
-getWatchHistory } from "../controllers/user.controller.js";
+     registerUser,
+     refreshAccessToken,
+     changeCurrentPassword,
+     getCurrentUser,
+     updateUserAvatar,
+     updateUserCoverImage,
+     getUserChannelProfile,
+     getWatchHistory,
+     updateAccountDetails
+     } from "../controllers/user.controller.js";
 import {upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -36,8 +37,8 @@ router.route("/register").post(
 
   router.route("/logout").post(verifyJWT,logoutUser)
   router.route("/refresh-token").post(refreshAccessToken)
-  router.route("/change-password").post(verifyJWT,changePassword)
-  router.router("/current-user").post(getCurrentUser)
+  router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+  router.route("/current-user").post(getCurrentUser)
   router.route("/update-account").patch(verifyJWT,updateAccountDetails)
   router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
   router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
